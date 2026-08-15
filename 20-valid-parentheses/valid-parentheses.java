50a707e4de0f58d69d34f66023e39b3f893971b8
ankit739 <ankitparashar739@gmail.com>
2026-08-15 15:38:17 +0530

    Time: 3 ms (86.11%) | Memory: 43.4 MB (41.49%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
     
-    Stack<Character> st = new Stack<>();
-    for(int i = 0;i<s.length();i++){
+     Stack<Character> st = new Stack<>();
+     for(int i =0;i<s.length();i++){
         char ch = s.charAt(i);
         if(ch=='('||ch=='{'||ch=='['){
             st.push(ch);
@@ -11,19 +11,19 @@ class Solution {
             if(st.isEmpty()){
                 return false;
             }
-            if((st.peek()=='('&&ch==')')|| (st.peek()=='{'&& ch=='}')||(st.peek()=='['&&ch==']')){
+            if((st.peek()=='('&&ch==')')||(st.peek()=='{'&&ch=='}')||(st.peek()=='['&&ch==']')){
                 st.pop();
             }
             else{
                 return false;
             }
         }
-    }
-    if(st.isEmpty()){
+     } 
+     if(st.isEmpty()){
         return true;
-    }
-    else{
+     }
+     else{
         return false;
-    }
+     }
     }
 }
